63c4ac9919c78d42ce0ff34cc7684f5b3555d492
miya0001 <miya@wpist.me>
2017-01-04 15:27:07 +0900

    bug fix on tests

--- tests/test-functions.php
+++ tests/test-functions.php
@@ -1,23 +1,9 @@
 <?php
 
-class SimpleMapTest extends WP_UnitTestCase
+class Vackup_Functions_Test extends WP_UnitTestCase
 {
 	/**
-	 * @test
-	 */
-	public function auth()
-	{
-		$res = Shifter_CLI::auth( "foo", "bar" );
-		$this->assertTrue( is_wp_error( $res ) );
-		$this->assertSame( "User does not exist.", $res->get_error_message() );
-
-		$res = Shifter_CLI::auth( getenv( "SHIFTER_USER" ), getenv( "SHIFTER_PASS" ) );
-		$this->assertTrue( ! is_wp_error( $res ) );
-		$this->assertTrue( !! $res->AccessToken );
-	}
-
-	/**
-	 * Tests for the `Shifter_CLI::rempty()`.
+	 * Tests for the `Vackup_Functions::rempty()`.
 	 *
 	 * @test
 	 * @since 0.1.0
@@ -25,38 +11,38 @@ class SimpleMapTest extends WP_UnitTestCase
 	public function rempty()
 	{
 		$dir = self::mockdir();
-		$files = Shifter_CLI::get_files( $dir );
+		$files = Vackup_Functions::get_files( $dir );
 		$this->assertSame( 7, iterator_count($files) );
 
 		$dir = self::mockdir();
-		Shifter_CLI::rempty( $dir );
-		$files = Shifter_CLI::get_files( $dir );
+		Vackup_Functions::rempty( $dir );
+		$files = Vackup_Functions::get_files( $dir );
 		$this->assertSame( 0, iterator_count($files) );
 
 		$dir = self::mockdir();
 
-		Shifter_CLI::rempty( $dir, array(
+		Vackup_Functions::rempty( $dir, array(
 			"dir02/dir02-01.txt",
 			"dir01/dir01-01/dir01-01-01.txt"
 		) );
-		$files = Shifter_CLI::get_files( $dir );
+		$files = Vackup_Functions::get_files( $dir );
 		$this->assertSame( 5, iterator_count($files) );
 	}
 
 	/**
-	 * Tests for the `Shifter_CLI::tempdir()`.
+	 * Tests for the `Vackup_Functions::tempdir()`.
 	 *
 	 * @test
 	 * @since 0.1.0
 	 */
 	public function tempdir()
 	{
-		$dir = Shifter_CLI::tempdir();
+		$dir = Vackup_Functions::tempdir();
 		$this->assertTrue( is_dir( $dir ) ); // $dir should exists.
 	}
 
 	/**
-	 * Tests for the `Shifter_CLI::rrmdir()`.
+	 * Tests for the `Vackup_Functions::rrmdir()`.
 	 *
 	 * @test
 	 * @since 0.1.0
@@ -66,12 +52,12 @@ class SimpleMapTest extends WP_UnitTestCase
 		$dir = self::mockdir();
 		$this->assertTrue( is_dir( $dir ) ); // $dir should exists.
 
-		Shifter_CLI::rrmdir( $dir );
+		Vackup_Functions::rrmdir( $dir );
 		$this->assertFalse( is_dir( $dir ) ); // $dir should not exists.
 	}
 
 	/**
-	 * Tests for the `Shifter_CLI::rcopy()`.
+	 * Tests for the `Vackup_Functions::rcopy()`.
 	 *
 	 * @test
 	 * @since 0.1.0
@@ -81,23 +67,23 @@ class SimpleMapTest extends WP_UnitTestCase
 		$src = self::mockdir();
 		$this->assertTrue( is_dir( $src ) ); // $dir should exists.
 
-		$dest = Shifter_CLI::tempdir();
+		$dest = Vackup_Functions::tempdir();
 		$this->assertTrue( is_dir( $dest ) ); // $dir should exists.
 		$this->assertTrue( self::md5sum( $src ) !== self::md5sum( $dest ) );
 
 		// Copy directory recursively then check md5.
-		Shifter_CLI::rcopy( $src, $dest );
+		Vackup_Functions::rcopy( $src, $dest );
 		$this->assertTrue( self::md5sum( $src ) === self::md5sum( $dest ) );
 
-		$dest = Shifter_CLI::tempdir();
-		Shifter_CLI::rcopy( $src, $dest, array( "dir01/dir01-01.txt" ) );
+		$dest = Vackup_Functions::tempdir();
+		Vackup_Functions::rcopy( $src, $dest, array( "dir01/dir01-01.txt" ) );
 		$this->assertFalse( is_file( $dest . '/dir01/dir01-01.txt' ) );
 		$this->assertTrue( is_file( $dest . '/dir01/dir01-02.txt' ) );
 		$this->assertTrue( is_file( $dest . '/dir02/dir02-01.txt' ) );
 	}
 
 	/**
-	 * Tests for the `Shifter_CLI::zip()`.
+	 * Tests for the `Vackup_Functions::zip()`.
 	 *
 	 * @test
 	 * @since 0.1.0
@@ -107,15 +93,15 @@ class SimpleMapTest extends WP_UnitTestCase
 		$src = self::mockdir();
 		$this->assertTrue( is_dir( $src ) ); // $dir should exists.
 
-		$dir = Shifter_CLI::tempdir();
+		$dir = Vackup_Functions::tempdir();
 
 		// zip $src
-		Shifter_CLI::zip( $src, $dir . '/archive.zip' );
+		Vackup_Functions::zip( $src, $dir . '/archive.zip' );
 		$this->assertTrue( is_file( $dir . '/archive.zip' ) );
 
 		// unzip to $dir . "/tmp"
 		mkdir( $dir . "/tmp" );
-		Shifter_CLI::unzip( $dir . '/archive.zip', $dir . "/tmp" );
+		Vackup_Functions::unzip( $dir . '/archive.zip', $dir . "/tmp" );
 		$this->assertTrue( self::md5sum( $src ) === self::md5sum( $dir . "/tmp" ) );
 	}
 
@@ -127,7 +113,7 @@ class SimpleMapTest extends WP_UnitTestCase
 	 */
 	public static function mockdir()
 	{
-		$dir = Shifter_CLI::tempdir();
+		$dir = Vackup_Functions::tempdir();
 		mkdir( $dir . "/dir01" );
 		file_put_contents( $dir . "/dir01/dir01-01.txt", time() );
 		file_put_contents( $dir . "/dir01/dir01-02.txt", time() );
@@ -152,7 +138,7 @@ class SimpleMapTest extends WP_UnitTestCase
 			return false;
 		}
 
-		$iterator = Shifter_CLI::get_files( $dir );
+		$iterator = Vackup_Functions::get_files( $dir );
 
 		$md5 = array();
 		foreach ( $iterator as $item ) {
